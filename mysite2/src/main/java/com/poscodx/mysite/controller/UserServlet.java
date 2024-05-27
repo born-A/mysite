7041b057600be13f23345328d53c8c95019548d0
born-A <93516595+born-A@users.noreply.github.com>
2024-05-27 17:49:11 +0900

    user update

--- mysite2/src/main/java/com/poscodx/mysite/controller/UserServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/UserServlet.java
@@ -5,8 +5,12 @@ import java.util.Map;
 import com.poscodx.mysite.controller.action.main.MainAction;
 import com.poscodx.mysite.controller.action.user.JoinAction;
 import com.poscodx.mysite.controller.action.user.JoinFormAction;
-import com.poscodx.mysite.controller.action.user.JoinSuccess;
+import com.poscodx.mysite.controller.action.user.JoinSuccessAction;
+import com.poscodx.mysite.controller.action.user.LoginAction;
 import com.poscodx.mysite.controller.action.user.LoginFormAction;
+import com.poscodx.mysite.controller.action.user.LogoutAction;
+import com.poscodx.mysite.controller.action.user.UpdateAction;
+import com.poscodx.mysite.controller.action.user.UpdateFormAction;
 
 public class UserServlet extends ActionServlet {
 	private static final long serialVersionUID = 1L;
@@ -14,8 +18,12 @@ public class UserServlet extends ActionServlet {
 	private Map<String, Action> mapAction = Map.of(
 		"joinform", new JoinFormAction(),
 		"join", new JoinAction(),
-		"joinsuccess", new JoinSuccess(),
-		"loginform", new LoginFormAction()
+		"joinsuccess", new JoinSuccessAction(),
+		"loginform", new LoginFormAction(),
+		"login", new LoginAction(),
+		"logout", new LogoutAction(),
+		"updateform", new UpdateFormAction(),
+		"update", new UpdateAction()
 	);
 	
 	@Override
--- mysite2/src/main/java/com/poscodx/mysite/controller/action/user/LogoutAction.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/action/user/LogoutAction.java
@@ -5,17 +5,23 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.poscodx.mysite.controller.ActionServlet.Action;
 
-public class JoinSuccess implements Action {
+public class LogoutAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request
-		.getRequestDispatcher("/WEB-INF/views/user/joinsuccess.jsp")
-		.forward(request, response);
+		HttpSession session = request.getSession();
+		if(session != null) {
+			/* 로그아웃 처리 */
+			session.removeAttribute("authUser");
+			session.invalidate();
+		}
 		
+		// redirect to main
+		response.sendRedirect(request.getContextPath());
 	}
 
 }
