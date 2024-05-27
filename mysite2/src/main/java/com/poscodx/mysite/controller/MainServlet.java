a78117e884121e1c06c94257a7175ce8d1eeb419
born-A <93516595+born-A@users.noreply.github.com>
2024-05-27 18:08:25 +0900

    GuestBook action 분리

--- mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
@@ -1,11 +1,5 @@
 package com.poscodx.mysite.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.poscodx.mysite.controller.action.main.MainAction;
 
 public class MainServlet extends ActionServlet {
@@ -15,14 +9,5 @@ public class MainServlet extends ActionServlet {
 	protected Action getAction(String actionName) {
 		return new MainAction();
 	}
-  
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		s
-//	}
-//
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//		doGet(request, response);
-//	}
 
 }
