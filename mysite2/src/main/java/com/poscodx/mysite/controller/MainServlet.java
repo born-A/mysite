a71a446d50f8189a1474c1d6d5123c6b6cc7ebdc
born-A <93516595+born-A@users.noreply.github.com>
2024-05-27 13:56:41 +0900

    servlet refactoring

--- mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
+++ mysite2/src/main/java/com/poscodx/mysite/controller/MainServlet.java
@@ -6,18 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MainServlet extends HttpServlet {
+import com.poscodx.mysite.controller.action.main.MainAction;
+
+public class MainServlet extends ActionServlet {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected Action getAction(String actionName) {
+		return new MainAction();
+	}
   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request
-			.getRequestDispatcher("/WEB-INF/views/main/index.jsp")
-			.forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		doGet(request, response);
-	}
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		s
+//	}
+//
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//		doGet(request, response);
+//	}
 
 }
