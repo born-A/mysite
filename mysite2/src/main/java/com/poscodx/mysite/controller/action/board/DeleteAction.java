daca5837dad69362ee355630efd85ff0008e6f15
born-A <93516595+born-A@users.noreply.github.com>
2024-05-30 20:30:57 +0900

    mysite - 게시물 삭제(delete)

---
+++ mysite2/src/main/java/com/poscodx/mysite/controller/action/board/DeleteAction.java
@@ -0,0 +1,42 @@
+package com.poscodx.mysite.controller.action.board;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.poscodx.mysite.controller.ActionServlet.Action;
+import com.poscodx.mysite.dao.BoardDao;
+import com.poscodx.mysite.dao.GuestbookDao;
+import com.poscodx.mysite.vo.UserVo;
+
+public class DeleteAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		// Access Control
+		if(session == null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
+		
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
+		
+		if(authUser == null) {
+			response.sendRedirect(request.getContextPath() + "/board?deleteSuccess=failed");
+			return;
+		}
+		
+		String sno = request.getParameter("no");
+		Long no = Long.parseLong(sno);
+		
+		String result = new BoardDao().deleteByNoAndUserNo(no, authUser.getNo());
+
+		response.sendRedirect(request.getContextPath() + "/board?deleteSuccess=" + result);
+	}
+
+}
