f73a794b7900c8665d4805d0a7d9d9d07839ddd3
born-A <93516595+born-A@users.noreply.github.com>
2024-06-11 17:33:13 +0900

    Site service, repo, vo, xml, interceptor

---
+++ mysite3/src/main/java/com/poscodx/mysite/vo/SiteVo.java
@@ -0,0 +1,11 @@
+package com.poscodx.mysite.vo;
+
+public class SiteVo {
+	private Long no;
+	private String title;
+	private String welcome;
+	private String profile;
+	private String description;
+	
+	
+}
